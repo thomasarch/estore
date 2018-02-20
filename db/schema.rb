ac96d40276a4935dd76a3ac5b85d7d80320dde8e
Thomas DeFelice <thomasarch@gmail.com>
2018-02-20 11:17:17 -0500

    make store front page add modal add products to db etc

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220132807) do
+ActiveRecord::Schema.define(version: 20180220144121) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "products", force: :cascade do |t|
@@ -27,6 +28,7 @@ ActiveRecord::Schema.define(version: 20180220132807) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
