5368adb62b2a0f75c0620501ec83eed84c31a631
Thomas DeFelice <thomasarch@gmail.com>
2018-02-21 09:23:46 -0500

    show by category show by brand etc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # before_action :authenticate_user!
+
+  before_action :categories, :brands
+
+  def categories
+  	@categories = Category.order(:name)
+  end
+
+  def brands
+  	@brands = Product.pluck(:brand).sort.uniq
+  end
 end
