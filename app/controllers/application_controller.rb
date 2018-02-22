f2f45c819260f7e2106e9de84931ef1141e8096f
Thomas DeFelice <thomasarch@gmail.com>
2018-02-22 00:10:21 -0500

    got the cart working somewhat

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,22 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :exception
 
   # before_action :authenticate_user!
 
-  before_action :categories, :brands
+	before_action :categories, :brands
 
-  def categories
-  	@categories = Category.order(:name)
-  end
+	before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def brands
-  	@brands = Product.pluck(:brand).sort.uniq
-  end
+	def configure_permitted_parameters
+	   devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+	   devise_parameter_sanitizer.permit(:account_update, keys: [:role])
+	end
+
+	def categories
+		@categories = Category.order(:name)
+	end
+
+	def brands
+		@brands = Product.pluck(:brand).sort.uniq
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221011653) do
+ActiveRecord::Schema.define(version: 20180221160703) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "quantity"
+    t.integer "line_item_total"
+    t.integer "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "subtotal"
+    t.decimal "sales_tax"
+    t.integer "grand_total"
+    t.integer "user_id"
+    t.text "order_items"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
@@ -44,6 +63,7 @@ ActiveRecord::Schema.define(version: 20180221011653) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
